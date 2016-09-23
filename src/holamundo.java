051dc024418ec7065c652a8914e79e2870b24f12
miguel192 <miguel.angel.g192@gmail.com>
2016-09-22 22:04:40 -0700

    segunda version

--- src/holamundo.java
+++ src/holamundo.java
@@ -8,7 +8,7 @@ public class holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-
+		System.out.println("miguel");
 	}
 
 }
